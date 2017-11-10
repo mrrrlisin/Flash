cfbe81c6821b55f8ebb1edbcd2460d2f15cab7b9
Mrrr Lisin <mrrrrlisin@gmail.com>
2017-11-10 16:38:17 +0300

    123

---
+++ Assets/Magic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic : MonoBehaviour 
+{
+    int goodNumber = 0;
+    public float speed = 0.5f;
+
+	void Start ()
+	{
+        Debug.Log("Пока");
+	}
+	
+	void Update ()
+	{
+        goodNumber = goodNumber + 1 ;
+        Debug.Log(goodNumber);    
+        if (Time.time > 2)
+        {
+            transform.Rotate(-speed, 0, 0);
+        }
+        else
+        {
+            transform.Rotate(speed, 0, 0);
+        }
+
+	}
+}
